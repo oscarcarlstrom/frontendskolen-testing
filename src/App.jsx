d98482f6220af80bd7a25c1c734a1a09df6e51cf
Oscar <oscar.carlstrom@bouvet.no>
2022-10-04 17:32:17 +0200

    Added API mock feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { updateBalance } from "./api/updateBalance";
 import "./App.css";
 import BankAccountAction from "./components/BankAccountAction";
 
 function App({ initialBalance = 0 }) {
 	const [currentBalance, setCurrentBalance] = useState(initialBalance);
+	const [userChangedBalance, setUserChangedBalance] = useState(initialBalance);
+
+	const [requestSuccess, setRequestSuccess] = useState();
+	const [requestError, setRequestError] = useState();
+
+	useEffect(() => {
+		if (!userChangedBalance) return; // React 18 dev mode workaround
+
+		setRequestSuccess(undefined);
+		setRequestError(undefined);
+
+		const { request, abortRequest } = updateBalance(currentBalance);
+		request
+			.then(({ amount }) => {
+				setRequestSuccess(`New blance was set to ${amount}!`);
+			})
+			.catch(({ message }) => {
+				setRequestError(message);
+			});
+
+		return abortRequest;
+	}, [currentBalance, userChangedBalance]);
+
+	const status = requestError ? (
+		<p className="error">
+			<strong>{requestError}</strong>
+		</p>
+	) : (
+		requestSuccess && (
+			<p className="success">
+				<strong>{requestSuccess}</strong>
+			</p>
+		)
+	);
 
 	return (
 		<div className="App">
@@ -11,10 +46,12 @@ function App({ initialBalance = 0 }) {
 			<h2>Your balance is {currentBalance}</h2>
 			<BankAccountAction
 				currentBalance={currentBalance}
-				setCurrentBalance={(amount) =>
-					setCurrentBalance((balance) => amount + balance)
-				}
+				setCurrentBalance={(amount) => {
+					setUserChangedBalance(true); // React 18 dev mode workaround
+					setCurrentBalance((balance) => amount + balance);
+				}}
 			/>
+			{status}
 		</div>
 	);
 }
---
+++ src/api/__mocks__/updateBalance.js
@@ -0,0 +1,1 @@
+export const updateBalance = (amount) => Promise.resolve({ amount });
